dac48ad464603f420a3e88e11866f266847a9268
Ben Williams <ben@negativeeddy.com>
2019-11-16 16:24:59 -0600

    style

--- src/LemonadeStand/Stand.cs
+++ src/LemonadeStand/Stand.cs
@@ -13,11 +13,13 @@
             /// Assets (cash on hand, in dollars)
             /// </summary>
             public decimal Assets { get; set; }
+
             /// <summary>
-            /// normally 1; 0 if everything is ruined by thunderstorm
-            /// originally G(i)
+            /// if everything is ruined by thunderstorm
+            /// originally G(i) : 0 = ruined, 1 = not ruined
             /// </summary>
-            public bool RuinedByThunderstorm { get; set; } // G(i): normally 1; 0 if everything is ruined by thunderstorm 
+            public bool RuinedByThunderstorm { get; set; }  
+
             public bool IsBankrupt { get; set; }
             public int L_GlassesMade { get; set; } // L(i): number of glasses of lemonade made by player i 
             public int P_PricePerGlassCents { get; set; } //P(i): Price charged for lemonade, per glass, in cents 
